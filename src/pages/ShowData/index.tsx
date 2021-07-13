1e29856d8cdde7a142234cbe3b30a69f4932b24a
Natalia <natyap28@hotmail.com>
2021-07-13 01:04:29 -0300

    add types

--- src/pages/ShowData/index.tsx
+++ src/pages/ShowData/index.tsx
@@ -1,31 +1,65 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import InputSearch from '../../components/InputSearch'
 import Card from '../../components/Card'
 import api from '../../services/api'
 import { Container, Title, CardSection } from './styles';
 
-const ShowData: React.FC = () => {
+interface LocationData {
+  name: string;
+}
+interface originData {
+  name: string;
+}
+interface CharacterData {
+  id?: number;
+  name: string;
+  image: string;
+  status: string;
+  species: string;
+  location: LocationData;
+  origin: originData;
+}
+
+const ShowData: React.FC<CharacterData> = (id) => {
+  const [characterList, setCharacterList] = useState([])
+  const [isloading, setIsLoading] = useState(false)
+
   useEffect(() => {
     async function fetchData() {
-      const { data } = await api.get('/character')
-      console.log(data.results[0])
+      try{ 
+        setIsLoading(true)
+        console.log('true')
+        const { data } = await api.get('/character')
+        setCharacterList(data.results)
+      } catch {
+        setIsLoading(false)
+        console.log('false')
+      }
     }
-
     fetchData()
   },[])
+
+  function loading() {
+   return <h1>loading</h1>
+  }
 
   return (
     <Container>
       <Title>Personagens</Title>
       <InputSearch />
-      <CardSection>
-       <Card />
-       <Card />
-       <Card />
-       <Card />
-       <Card />
-       <Card />
+      {!isloading ? (
+        loading() 
+      ):(
+        <CardSection>
+        {characterList.map((character, key) => (
+          <Card 
+          key={key}
+          character={character}
+          />
+        ))}
       </CardSection>
+      )}
+      
     </Container>
   )
 }
